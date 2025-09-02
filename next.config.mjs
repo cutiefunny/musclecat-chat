c516045e8d80ca82e7b9014f0e550928007e8342
cutiefunny <cutiefunny@gmail.com>
2025-09-02 10:05:43 +0900

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+import withPWA from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    // 💡 AVIF와 WebP 포맷을 사용하도록 설정 추가
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+};
+
+export default withPWA({
+  dest: "public",
+})(nextConfig);
