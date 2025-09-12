fa22f3f9b78818d0aa1006d9dd165d47cf5f843e
cutiefunny <cutiefunny@gmail.com>
2025-09-12 23:22:16 +0900

    re

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,9 +3,15 @@
 importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js");
 
-// 💡 URLSearchParams를 사용하여 쿼리 문자열에서 설정 값을 가져옵니다.
-const urlParams = new URLSearchParams(location.search);
-const firebaseConfig = Object.fromEntries(urlParams.entries());
+// 💡 Vercel 환경 변수에서 직접 설정 값을 가져오도록 수정합니다.
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
 
 
 firebase.initializeApp(firebaseConfig);
